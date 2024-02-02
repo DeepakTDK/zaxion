044a3729fcf35fc33274ffaf4a0368f6a3feae60
DeepakTDK <deepakdheepu25@gmail.com>
2024-02-02 15:12:08 +0530

    v1.0

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-black">
+      <div className="text-center">
+        <p className="text-5xl text-red-600">Zaxion</p>
+        <h1 className="text-3xl text-red-600">The low code marketplace</h1>
+        <br />
+        <img className="rounded-lg" src="src\assets\cook.jpg" alt="Cook" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
